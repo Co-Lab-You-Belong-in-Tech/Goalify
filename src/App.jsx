3892a4a9a4b06d39e238a561f454b1ae5d239a10
Bishoy <bishoy.samwuel@gmail.com>
2022-10-15 20:00:11 +0200

    refactor: center the main content

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,11 @@
-import BasicModal from "./components/modal.jsx";
+import InitialGoalMessage from "./components/InitialGoalMessage.jsx";
 
 function App() {
-  return <div className="App">
-    <BasicModal />
-  </div>;
+    return <div className="App" style={{
+        display: 'flex', justifyContent: 'center', alignItems: 'center', height: '100vh',
+    }}>
+        <InitialGoalMessage/>
+    </div>;
 }
 
 export default App;
