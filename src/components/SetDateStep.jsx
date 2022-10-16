1317cf520573280ae18b36b555945148ed35456b
Bishoy <bishoy.samwuel@gmail.com>
2022-10-16 15:51:08 +0200

    feat: setup the assign dates step

--- src/components/SetDateStep.jsx
+++ src/components/SetDateStep.jsx
@@ -1,11 +1,45 @@
+import TextField from '@mui/material/TextField';
+import { AdapterDayjs } from '@mui/x-date-pickers/AdapterDayjs';
+import { LocalizationProvider } from '@mui/x-date-pickers/LocalizationProvider';
+import { DatePicker } from '@mui/x-date-pickers/DatePicker';
 import Typography from "@mui/material/Typography";
+import {useState} from "react";
+import Box from "@mui/material/Box";
+import {Grid} from "@mui/material";
 
 function SetDateStep() {
-    return (
-        <Typography variant="h6" component="h2">
-            Does this goal have a start and end date?
-        </Typography>
-        )
+    const [startDate, setStartDate] = useState(null);
+    const [endDate, setEndDate] = useState(null);
+    return (<Box>
+            <Typography variant="h6" gutterBottom>
+                Great, dates help keep momentum with our goals!
+                Pick a start and end date below
+            </Typography>
+            <Grid>
+                <LocalizationProvider dateAdapter={AdapterDayjs}>
+                    <DatePicker
+                        label="Start Date"
+                        value={startDate}
+                        onChange={(newValue) => {
+                            setStartDate(newValue);
+                        }}
+                        renderInput={(params) => <TextField {...params} />}
+                    />
+                </LocalizationProvider>
+                <LocalizationProvider dateAdapter={AdapterDayjs}>
+                    <DatePicker
+                        label="End Date"
+                        value={endDate}
+                        onChange={(newValue) => {
+                            setEndDate(newValue);
+                        }}
+                        renderInput={(params) => <TextField {...params} />}
+                    />
+                </LocalizationProvider>
+            </Grid>
+        </Box>
+
+    )
 
 }
 
