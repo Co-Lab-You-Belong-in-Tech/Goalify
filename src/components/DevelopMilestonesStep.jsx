f4c0418a3d64afe75c19a3a9062c9afc4115b147
Bishoy <bishoy.samwuel@gmail.com>
2022-10-16 15:52:19 +0200

    style: add style to develop milestone step

--- src/components/DevelopMilestonesStep.jsx
+++ src/components/DevelopMilestonesStep.jsx
@@ -5,23 +5,42 @@ import {useState} from "react";
 function CreateNewGoal() {
     const [milestones, setMilestones] = useState([])
     const [milestoneInput, setMileStoneInput] = useState('')
-    const milestone = (milestoneConent) => <Grid xs display="flex" justifyContent="center" alignItems="center"
+    const milestone = ({milestoneContent, number}) => <Grid xs display="flex" flexDirection="column" justifyContent="center" alignItems="center"
                                                  sx={{
-                                                     height: 100, border: '#f5f5f5 1px  solid', '&:hover': {
-                                                         opacity: [0.9, 0.8, 0.7],
-                                                     },
+                                                     display: "flex",
+                                                     flexDirection: "column",
+                                                     alignItems: "flex-start",
+                                                     padding: "16px",
+                                                     margin: "5px",
+                                                     gap: "16px",
+
+                                                     width: "1090px",
+                                                     height: "172px",
+                                                     background: "#FFFFFF",
+                                                     border: "1px solid rgba(65, 84, 114, 0.25)",
+                                                     borderRadius: "4px",
                                                  }}
     >
+        <Typography
+            sx={{
+                borderRadius: "50%",
+                border: 'grey 1px  solid',
+                padding: '5px',
+            }}
+            alignSelf="start" variant="caption" display="block" gutterBottom>
+            {String(number).padStart(2, '0')}
+        </Typography>
         <TextField id="filled-basic"
                    fullWidth
                    sx={{
-                       border: 'none'
+                       border: '0px',
+                       background: "#F5F5F5",
                    }}
                    mb={1}
-                   value={milestoneConent || milestoneInput}
-                   onChange={(e) => setMileStoneInput(e.target.value)}/>
+                   value={milestoneContent || milestoneInput}
+                   onChange={(e) => milestoneContent || setMileStoneInput(e.target.value)}/>
     </Grid>
-    return (<box>
+    return <box>
             <Typography variant="h6" gutterBottom>
                 Developing your new goal
             </Typography>
@@ -39,18 +58,27 @@ function CreateNewGoal() {
                     height:100
                 }}
             >
-                {milestones.map((m => (milestone(m.content))))}
-                {milestone()}
+                {milestones.map((m, i) => (milestone({milestoneContent: m.content, number:i+1})))}
+                {milestone({number: milestones.length+1})}
             </grid>
             <Button variant="outlined" size="large" fullWidth
+                    sx={{
+                        display: "flex",
+                        flexDirection: "row",
+                        justifyContent: "center",
+                        alignItems: "center",
+                        padding: "16px",
+                        gap: "10px",
+                        height: "56px",
+                        background: "#F4F6FF",
+                        borderRadius: "50px",
+                    }}
                     onClick={() => {
                         setMilestones([...milestones, {content: milestoneInput}]); setMileStoneInput('')
                     }}>
                 + Add milestone
             </Button>
         </box>
-
-    )
 
 }
 
