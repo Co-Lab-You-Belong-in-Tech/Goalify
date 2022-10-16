75673ef58e1301d6cb8993bbf981528fba9ffdf6
Bishoy <bishoy.samwuel@gmail.com>
2022-10-16 14:05:11 +0200

    refactor: add text input and description to Create goal step

--- src/components/CreateNewGoalStep.jsx
+++ src/components/CreateNewGoalStep.jsx
@@ -1,10 +1,20 @@
 import Typography from "@mui/material/Typography";
+import {TextField} from "@mui/material";
 
 function CreateNewGoalStep() {
     return (
-        <Typography variant="h6" component="h2">
-            Creating your new goal
-        </Typography>
+        <div>
+            <Typography variant="h6" gutterBottom>
+                What is your goal?
+            </Typography>
+            <TextField placeholder="ex: I want to learn how to ride a bike so I can commute around the city & reduce my contribution to air pollution" fullWidth  />
+            <Typography variant="h6" gutterBottom>
+                Why do you want to achieve this goal?
+            </Typography>
+            <TextField placeholder="ex: I want to learn how to ride a bike so I can commute around the city & reduce my contribution to air pollution" fullWidth />
+        </div>
+
+
         )
 
 }
