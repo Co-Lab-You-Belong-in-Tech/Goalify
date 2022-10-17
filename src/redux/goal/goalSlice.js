680945c96cf6e5536ab55232d7a553613ab0af5f
Bishoy <bishoy.samwuel@gmail.com>
2022-10-17 19:38:45 +0200

    chore: remove the goals of initial state

--- src/redux/goal/goalSlice.js
+++ src/redux/goal/goalSlice.js
@@ -1,18 +1,6 @@
 import {createSlice} from '@reduxjs/toolkit';
 
-const initialState = [{
-    id: 1, goal: 'Est elit dolor eiusmod labore conse',
-}, {id: 2, goal: 'Exercitation nulla cupidatat ea ipsum. '}, {id: 3, goal: 'Ut exercitation exercitation'}, {
-    id: 4, goal: 'Id qui nulla laborum irure esse',
-}, {
-    id: 5, goal: 'Adipisicing aliquip nostrud',
-}, {
-    id: 6, goal: 'Occaecat consequat laboris reprehenderit',
-}, {
-    id: 7, goal: 'Enim deserunt magna sint officia',
-}, {
-    id: 8, goal: 'Culpa anim voluptate Lorem',
-},];
+const initialState = [];
 
 const goalSlice = createSlice({
     name: 'goals', initialState, reducers: {
