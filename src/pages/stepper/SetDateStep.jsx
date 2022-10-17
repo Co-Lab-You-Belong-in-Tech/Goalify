bd0ae28819d6dbbfff80f30b43c75bc0751c11d1
Bishoy <bishoy.samwuel@gmail.com>
2022-10-17 12:10:01 +0200

    feat: add dates to goal object

--- src/pages/stepper/SetDateStep.jsx
+++ src/pages/stepper/SetDateStep.jsx
@@ -1,45 +1,49 @@
 import TextField from '@mui/material/TextField';
-import { AdapterDayjs } from '@mui/x-date-pickers/AdapterDayjs';
-import { LocalizationProvider } from '@mui/x-date-pickers/LocalizationProvider';
-import { DatePicker } from '@mui/x-date-pickers/DatePicker';
+import {AdapterDayjs} from '@mui/x-date-pickers/AdapterDayjs';
+import {LocalizationProvider} from '@mui/x-date-pickers/LocalizationProvider';
+import {DatePicker} from '@mui/x-date-pickers/DatePicker';
 import Typography from "@mui/material/Typography";
 import {useState} from "react";
 import Box from "@mui/material/Box";
-import {Grid} from "@mui/material";
 
-function SetDateStep() {
+function SetDateStep({setGoal}) {
     const [startDate, setStartDate] = useState(null);
     const [endDate, setEndDate] = useState(null);
-    return (<Box>
-            <Typography variant="h6" gutterBottom>
-                Great, dates help keep momentum with our goals!
-                Pick a start and end date below
-            </Typography>
-            <Grid>
-                <LocalizationProvider dateAdapter={AdapterDayjs}>
-                    <DatePicker
-                        label="Start Date"
-                        value={startDate}
-                        onChange={(newValue) => {
-                            setStartDate(newValue);
-                        }}
-                        renderInput={(params) => <TextField {...params} />}
-                    />
-                </LocalizationProvider>
-                <LocalizationProvider dateAdapter={AdapterDayjs}>
-                    <DatePicker
-                        label="End Date"
-                        value={endDate}
-                        onChange={(newValue) => {
-                            setEndDate(newValue);
-                        }}
-                        renderInput={(params) => <TextField {...params} />}
-                    />
-                </LocalizationProvider>
-            </Grid>
-        </Box>
 
-    )
+    return (<Box>
+        <Typography variant="h6" gutterBottom>
+            Great, dates help keep momentum with our goals!
+            Pick a start and end date below
+        </Typography>
+        <LocalizationProvider dateAdapter={AdapterDayjs}>
+            <DatePicker
+                label="Start Date"
+                value={startDate}
+                onChange={(newValue) => {
+                    setStartDate(newValue);
+                    setGoal((prev) => {
+                        return {...prev, startDate: newValue}
+                    })
+                    console.log(newValue)
+                }}
+                renderInput={(params) => <TextField {...params} />}
+            />
+        </LocalizationProvider>
+        <LocalizationProvider dateAdapter={AdapterDayjs}>
+            <DatePicker
+                label="Start Date"
+                value={endDate}
+                onChange={(newValue) => {
+                    setEndDate(newValue);
+                    setGoal((prev) => {
+                        return {...prev, endDate: newValue}
+                    })
+                    console.log(newValue)
+                }}
+                renderInput={(params) => <TextField {...params} />}
+            />
+        </LocalizationProvider>
+    </Box>);
 
 }
 
