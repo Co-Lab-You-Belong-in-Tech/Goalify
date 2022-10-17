a3f7bff19feba7e7285f9eb623598eac8b23738c
Bishoy <bishoy.samwuel@gmail.com>
2022-10-17 15:48:25 +0200

    style: style the initialGoalMessage

--- src/pages/stepper/InitialGoalMessage.jsx
+++ src/pages/stepper/InitialGoalMessage.jsx
@@ -4,7 +4,7 @@ import Button from '@mui/material/Button';
 import Modal from '@mui/material/Modal';
 import HorizontalLinearStepper from "./stepper.jsx";
 import {Grid} from "@mui/material";
-import Typography from "@mui/material/Typography";
+import emoji from "../../assets/icons/emoji.svg";
 
 const style = {
     position: 'absolute',
@@ -23,24 +23,28 @@ export default function InitialGoalMessage() {
     const handleOpen = () => setOpen(true);
     const handleClose = () => setOpen(false);
 
-    return (
-        <Grid display="flex" flexDirection="column" justifyContent="center" alignItems="center" style={{
-            display: 'flex', justifyContent: 'center', alignItems: 'center', height: '100vh',
-        }}>
-            <Typography variant="h6" gutterBottom>
-                Looks like you don’t have any goals just yet....let’s get that sorted
-            </Typography>
-            <Button onClick={handleOpen}>+ Create A Goal</Button>
-            <Modal
-                open={open}
-                onClose={handleClose}
-                aria-labelledby="modal-modal-title"
-                aria-describedby="modal-modal-description"
-            >
-                <Box sx={style}>
-                    <HorizontalLinearStepper />
-                </Box>
-            </Modal>
-        </Grid>
-    );
+    return (<Grid display="flex" flexDirection="column" justifyContent="center" alignItems="center" height="100vh" sx={{background:"#FFFFF9"}}>
+        <p className="text-4xl leading-10 font-bold mb-10"> Welcome, Jason</p>
+
+        <div className="border divide-neutral-300 border-solid p-5 bg-white	">
+            <Grid display="flex" flexDirection="column" justifyContent="center" alignItems="center">
+
+                <p className="text-2xl font-bold leading-9 ">
+                    Looks like you don’t have any goals just yet....let’s get that sorted
+                </p>
+                <Button sx={{background: "#F4F6FF", marginY: "15px"}} onClick={handleOpen}>+ Create A Goal</Button>
+                <img width={100} height={100} src={emoji} alt="edit" className="pr-2"/>
+            </Grid>
+        </div>
+        <Modal
+            open={open}
+            onClose={handleClose}
+            aria-labelledby="modal-modal-title"
+            aria-describedby="modal-modal-description"
+        >
+            <Box sx={style}>
+                <HorizontalLinearStepper/>
+            </Box>
+        </Modal>
+    </Grid>);
 }
