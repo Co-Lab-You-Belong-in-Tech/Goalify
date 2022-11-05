73da6c2862d8900df54e4b08593f4f7bb7332251
Bishoy <bishoy.samwuel@gmail.com>
2022-11-05 19:33:46 +0200

    fix: fix logical syntax

--- src/pages/goalPage/Milestone.jsx
+++ src/pages/goalPage/Milestone.jsx
@@ -52,7 +52,7 @@ const Milestone = () => {
                           completed: false,
                         };
                       } else {
-                        return { milestone };
+                        return  milestone ;
                       }
                     });
                     dispatch(
