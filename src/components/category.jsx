30d07adeccecbc3bf0af1964f5f7ea82083d10f6
Bishoy <bishoy.samwuel@gmail.com>
2022-11-06 20:29:51 +0200

    feat: add changing category feature

--- src/components/category.jsx
+++ src/components/category.jsx
@@ -1,25 +1,83 @@
 import React, { useState } from 'react';
 import { useSelector, useDispatch } from 'react-redux';
-import {modifyCategories, updateSelected} from '../redux/category/categorySlice';
+import {modifyCategories, modifyCategory, updateSelected} from '../redux/category/categorySlice';
+import dots from '../assets/icons/dots.svg'
+
+
 import { plusI } from '../assets/icons';
 
 const category = () => {
   const dispatch = useDispatch();
   const { categories, selected } = useSelector((state) => state.categories);
+  const [isEditing, setIsEditing] = useState(false);
+  const [value, setValue] = useState()
+
+  const handleClick = () => {
+    setIsEditing(true);
+  };
+  const handleKeyPress = e => {
+    if (e.key === "Enter") {
+      setIsEditing(false);
+      const newCategories = categories.map((c) => {
+        console.log({i:categories.indexOf(c), selected})
+        if (categories.indexOf(c) === selected) {
+          return {
+            ...c, category: value,
+          };
+        }
+
+        return c;
+      })
+      console.log(newCategories)
+      dispatch(modifyCategory(newCategories));
+    }
+  };
+
+  const onChange = (e) => {
+    setValue(e.target.value)
+  }
+
   return (
     <div className="flex gap-3 w-full rounded-full p-1 bg-slate-100">
       {categories.map(({ category, id }) => (
-        <p
-          className={`py-1 px-2 ${
-            selected === id ? 'bg-white rounded-full' : ''
-          }`}
-          key={id}
-          onClick={() => dispatch(updateSelected(id))}
-        >
-          {category}
-        </p>
+          isEditing && id == selected? (
+                <input
+                    autoFocus
+                    placeholder={category}
+                    onChange={onChange}
+                    onKeyPress={handleKeyPress}
+                    className={"py-1 px-2 border-solid border border-gray-100 rounded-3xl "}
+                    type="text"
+                />
+            ) : (
+              <div
+                className={`py-1 px-2 hover:cursor-pointer flex justify-between w-[10rem] ${
+                  selected === id ? 'bg-white border-solid border border-gray-200 rounded-3xl ' : ''
+                }`}
+                key={id}
+                onClick={()=>dispatch(updateSelected(id))}
+
+
+              >
+                {category}
+                {selected === id ?
+                <img className={"w-8"} src={dots} onClick={()=>{
+                  handleClick();
+                  dispatch(updateSelected(id))
+                }}/> : null }
+              </div>
+            )
+
       ))}
-      <img src={plusI} onClick={()=> {dispatch(modifyCategories([...categories, {id: Math.random * 1000, category: 'New Category'}]))}} />
+      <img src={plusI} onClick={()=> {
+        dispatch(modifyCategories(
+            [...categories,
+              {id: categories.length, category: 'New Category'}]));
+        dispatch(updateSelected(categories.length-1)
+        );
+      }
+
+      } />
     </div>
   );
 };
