56214adeffed2bf2dad92aebc8d0871499a3b65e
Omar Salem <omarsalem0721@gmail.com>
2022-11-07 14:23:52 +0200

    Add storage to catagory

--- src/redux/category/categorySlice.js
+++ src/redux/category/categorySlice.js
@@ -1,36 +1,42 @@
 import { createSlice } from '@reduxjs/toolkit';
 
-const initialState = {
-  selected: 0,
-  categories: [
-    {
-      id: 0,
-      category: 'Active goals',
-    },
-    {
-      id: 1,
-      category: 'Paused goals',
-    },
-  ],
-};
-
+const initialState = localStorage.getItem('category')
+  ? JSON.parse(localStorage.getItem('category'))
+  : {
+      selected: 0,
+      categories: [
+        {
+          id: 0,
+          category: 'Active goals',
+        },
+        {
+          id: 1,
+          category: 'Paused goals',
+        },
+      ],
+    };
+localStorage.setItem('category', JSON.stringify(initialState));
 const categorySlice = createSlice({
   name: 'categories',
   initialState,
   reducers: {
     updateSelected(state, action) {
       state.selected = action.payload;
+      localStorage.setItem('category', JSON.stringify(state));
     },
     modifyCategories(state, action) {
       state.categories = [...action.payload];
+      localStorage.setItem('category', JSON.stringify(state));
     },
     modifyCategory(state, action) {
-       state.categories = action.payload;
+      state.categories = action.payload;
+      localStorage.setItem('category', JSON.stringify(state));
     },
   },
 });
 
 // export actions
-export const { updateSelected, modifyCategories, modifyCategory } = categorySlice.actions;
+export const { updateSelected, modifyCategories, modifyCategory } =
+  categorySlice.actions;
 
 export default categorySlice.reducer;
