5f4f19e2b748589123e7fe4cf2a2b4b18f629782
Bishoy <bishoy.samwuel@gmail.com>
2022-12-01 19:31:11 +0200

    chore: fix spacing

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { createBrowserRouter, RouterProvider } from 'react-router-dom';
-import { Provider } from 'react-redux';
+import {createBrowserRouter, RouterProvider} from 'react-router-dom';
+import {Provider} from 'react-redux';
 import store from './redux/store';
 import ErrorPage from './pages/error';
 import Dashboard from './pages/dashboard/dashboard';
@@ -13,34 +13,34 @@ import GoalPage from './pages/goalPage/GoalPage.jsx';
 import InitialGoalMessage from './components/stepper/InitialGoalMessage.jsx';
 
 const router = createBrowserRouter([
-  {
-    path: '/',
-    element: <Dashboard />,
-    errorElement: <ErrorPage />,
-  },
-  {
-    path: '/login',
-    element: <Login />,
-  },
-  {
-    path: '/signup',
-    element: <Signup />,
-  },
-  {
-    path: '/createGoal',
-    element: <InitialGoalMessage />,
-    errorElement: <ErrorPage />,
-  },
-  {
-    path: '/goals/:goalId',
-    element: <GoalPage />,
-  },
+    {
+        path: '/',
+        element: <Dashboard/>,
+        errorElement: <ErrorPage/>,
+    },
+    {
+        path: '/login',
+        element: <Login/>,
+    },
+    {
+        path: '/signup',
+        element: <Signup/>,
+    },
+    {
+        path: '/createGoal',
+        element: <InitialGoalMessage/>,
+        errorElement: <ErrorPage/>,
+    },
+    {
+        path: '/goals/:goalId',
+        element: <GoalPage/>,
+    },
 ]);
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <Provider store={store}>
-    <React.StrictMode>
-      <RouterProvider router={router} />
-    </React.StrictMode>
-  </Provider>
+    <Provider store={store}>
+        <React.StrictMode>
+            <RouterProvider router={router}/>
+        </React.StrictMode>
+    </Provider>
 );
