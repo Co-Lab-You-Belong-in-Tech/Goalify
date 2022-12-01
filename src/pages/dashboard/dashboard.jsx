885949918a11f159724e95acd1e647158460f96a
Bishoy <bishoy.samwuel@gmail.com>
2022-12-01 19:34:39 +0200

    feat: show stepper

--- src/pages/dashboard/dashboard.jsx
+++ src/pages/dashboard/dashboard.jsx
@@ -1,14 +1,23 @@
 import React from 'react';
 import Header from './header';
 import ListCards from './listCards';
+import {useSelector} from "react-redux";
+import IntroduceStepper from "../../components/stepper/IntroduceStepper.jsx";
 
 const Dashboard = () => {
-  return (
-    <>
-      <Header />
-      <ListCards />
-    </>
-  );
+    const name = useSelector((state) => state.user.name);
+    return (
+        <>
+            { (name.length === 0) ?
+                <IntroduceStepper/>
+                : <>
+                    <Header/>
+                    <ListCards/>
+                </>
+            }
+
+        </>
+    );
 };
 
 export default Dashboard;
