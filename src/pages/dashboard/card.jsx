c03c66fdc8a8089be053b716835a2a65c9d2dd24
Ade <mymail@gmail.comadeobayomi@gmail.com>
2022-12-15 14:24:32 -0500

    re truncation

--- src/pages/dashboard/card.jsx
+++ src/pages/dashboard/card.jsx
@@ -193,7 +193,7 @@ const Card = ({ goal }) => {
             ? <b><i>Congratulations</i> 🥳🎉🎊 </b>
             : `Milestone   ${getCurrentMilestone + 1}: ${goal?.milestones[
                 getCurrentMilestone
-              ]?.content?.substring(0, 31)}...`}
+              ]?.content?.substring(0, 30)}...`}
           {isAlertVisible && <Celebrate />}
         </p>
       </div>
